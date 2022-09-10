28507e1b34ad62d3da5203a3d5e6c0554ae2c91f
Gowri Shankaran A.N <angs@ANGS-Air.local>
2022-09-10 21:15:05 +0530

    Added App component

--- App.js
+++ App.js
@@ -8,3 +8,8 @@
 
 import React from 'react';
 
+const App = () => {
+    return null
+}
+
+export default App;
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,3 +4,13 @@
 
 import 'react-native';
 import React from 'react';
+import App from '../App';
+import {render} from '@testing-library/react-native'
+
+describe("App", () => {
+
+  it("App component renders", () => {
+    const appInstance = render(<App/>)
+    expect(appInstance).toBeDefined()
+  })
+})
